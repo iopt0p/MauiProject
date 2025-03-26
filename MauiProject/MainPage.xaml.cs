786fed10606418ddd36164437ecf8bb083c166d7
Roman Kravchenko <rkravchenko466@gmail.com>
2025-03-26 19:38:31 +0200

    Uploading start version with task1

---
+++ MauiProject/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace MauiProject;
+
+public partial class MainPage : ContentPage
+{
+    int count = 0;
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+        count++;
+
+        if (count == 1)
+            CounterBtn.Text = $"Clicked {count} time";
+        else
+            CounterBtn.Text = $"Clicked {count} times";
+
+        SemanticScreenReader.Announce(CounterBtn.Text);
+    }
+
+    private void Task1Button_OnClicked(object? sender, EventArgs e)
+    {
+        Navigation.PushAsync(new Task1());
+    }
+}
---
+++ MauiProject/Task1.xaml.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiProject;
+
+public partial class Task1 : ContentPage
+{
+    public Task1()
+    {
+        InitializeComponent();
+    }
+
+    private void Variable_OnTextChanged(object? sender, TextChangedEventArgs e)
+    {
+        if (string.IsNullOrWhiteSpace(e.NewTextValue)) return;
+
+        if (!double.TryParse(e.NewTextValue, out double value))
+        {
+            ((Entry) sender).Text = e.OldTextValue;
+        }
+
+    }
+
+    private void CalculateButton_OnClicked(object? sender, EventArgs e)
+    {
+        if (string.IsNullOrEmpty(Variable1.Text) || string.IsNullOrEmpty(Variable2.Text))
+        {
+            Result.Text = "Please enter a value";
+            return;
+        }
+
+        double variable1 = double.Parse(Variable1.Text);
+        double variable2 = double.Parse(Variable2.Text);
+
+        Result.Text = Convert.ToDecimal((6 - Math.Cos(3 + variable1))
+                                        / (34 - 9 * Math.Pow(variable2, 3) + variable2)).ToString();
+
+        Average.Text = ((variable1 + variable2) / 2).ToString();
+    }
+
+    private void ResetButton_OnClicked(object? sender, EventArgs e)
+    {
+        Variable1.Text = string.Empty;
+        Variable2.Text = string.Empty;
+        Result.Text = string.Empty;
+        Average.Text = string.Empty;
+    }
+
+    private void ExitButton_OnClicked(object? sender, EventArgs e)
+    {
+        App.Current.Quit();
+    }
+}
